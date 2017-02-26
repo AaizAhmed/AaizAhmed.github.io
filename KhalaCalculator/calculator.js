43e85aaf22ed3bb97f8047a96d057600d987b4a6
Aaiz Ahmed <aaiza2@umbc.edu>
2017-02-25 22:58:39 -0500

    done

---
+++ KhalaCalculator/calculator.js
@@ -0,0 +1,65 @@
+
+function addMore()
+{
+	var str = '<tr> <td> <input type="text"> </td>';
+		str += '<td> <input type="text"> </td>';
+		str += '<td> <input type="text"> </td></tr>';		   
+			  
+	for (var i = 0; i < 5; i++) 
+	{
+		$('#inputDisplay').append( str );				
+	}
+}
+
+function doCalculation()
+{
+
+	var table = document.getElementById('inputDisplay');
+
+	var total = 0
+
+    for (var r = 1, n = table.rows.length; r < n; r++) 
+    {
+       for (var c = 1; c < 2; c++) 
+       {
+           var num = table.rows[r].cells[c].children[0].value;
+
+           var regEx = /[^\d]/g;   
+
+           if ( num.match(regEx) )
+           {
+           		alert("Wrong input: Row " + r + " Col 2");
+           		return;
+           		console.log("Test passed!");
+           		console.log(num);
+           }    
+           
+           if (num != "")
+           {
+           		num += '.';
+
+           		var tmp = table.rows[r].cells[c+1].children[0].value;
+
+           		if (tmp.match(regEx))
+           		{
+           			alert("Wrong input: Row " + r + " Col 3");
+           			return;
+           		}
+
+           		num += tmp;
+
+           		total += roundTwo(num);
+           }
+       }
+    }
+
+    $("#result").html("<u> Total: " + total + "</u>");
+}
+
+$(document).ready( function()
+{
+	var d = new Date();
+	var str = d.getMonth()+1 + '-' + d.getDate() + '-' + d.getFullYear();;
+
+	$('#date').append(str);
+});
